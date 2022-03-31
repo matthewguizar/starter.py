9b8ed2b20bc254c89b5c183c455ef633750bed2d
Matthew Guizar <matthewrguizar@gmail.com>
2022-03-31 12:37:51 -0700

    finished

--- data_presenter.py
+++ data_presenter.py
@@ -8,12 +8,17 @@ cup_invoices = open("CupcakeInvoices.csv")
 #     print(flavor[2])
 
 
-for price in cup_invoices:
-    price = price.split(',')
-    total = int(price[3]) * float(price[4])
+# for i_price in cup_invoices:
+#     i_price = i_price.split(',')
+#     i_total = int(i_price[3]) * float(i_price[4])
 
-    # print(total)
+    # print(i_total)
     
 total = 0
 
-for 
+# for t_price in cup_invoices:
+#     t_price = t_price.split(',')
+#     total = total + int(t_price[3]) * float(t_price[4])
+# print(total)
+
+cup_invoices.close()
