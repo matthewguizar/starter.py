5a6fd53669bfea52875632fc7434774e05f7a3d1
Matthew Guizar <matthewrguizar@gmail.com>
2022-03-31 12:11:22 -0700

    added checking for all the cupcake flavors

--- data_presenter.py
+++ data_presenter.py
@@ -2,3 +2,8 @@ cup_invoices = open("CupcakeInvoices.csv")
 
 # for all_rows in cup_invoices:
 #     print (all_rows)
+
+# for flavor in cup_invoices:
+#     flavor = flavor.rstrip('\n').split(',')
+#     print(flavor[2])
+
